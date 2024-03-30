35b1f962a753c42e027cc8a9bc888aea9fef2821
Kelly Santos <kellycristinasntos@gmail.com>
2024-03-30 14:11:29 -0300

    teste rota

--- app.py
+++ app.py
@@ -4,3 +4,32 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+livros = [
+    {
+        "id": 0,
+        "nome" : "Titulo 1",
+        "autor" : "Anne"
+    },
+    {
+        "id": 1,
+        "nome" : "Titulo 2",
+        "autor" : "Sakura"
+    },
+    {
+        "id": 2,
+        "nome" : "Titulo 3",
+        "autor" : "Solar"
+    },
+    {
+        "id": 3,
+        "nome" : "Titulo 4",
+        "autor" : "Daya"
+    },
+]
+
+@app.route('/')
+def get_livros():
+    return jsonify(livros)
+
+app.run(port=5000, host='localhost', debug=True)
